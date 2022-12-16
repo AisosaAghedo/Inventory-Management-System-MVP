2295af38eaee5db956ceedbf2362d4de937bc2c8
Haywhiz <babarindeademola@gmail.com>
2022-12-16 20:19:24 +0100

    basemodel works

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,7 @@
 
 import models
 
- def __init__(self, *args, **kwargs):
+def __init__(self, *args, **kwargs):
         """Instantiation of base model class
         Args:
             args: it won't be used
